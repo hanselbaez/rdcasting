11bcd30917d585226bcb00e4c86f60dcd9af00eb
Hansel Baez <57023185+hanselbaez@users.noreply.github.com>
2020-02-11 16:05:53 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,12 @@
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
+//Slider Home
+$(document).ready(function(){
+$("#news-slider13").owlCarousel({
+  navigation : false,
+  pagination : true,
+  items : 3,
+  itemsDesktop:[1199,3],
+  itemsDesktopSmall:[980,2],
+  itemsMobile : [600,1],
+  navigationText : ["",""]
 });
-
-
-
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
+});
--- js.js
+++ js.js
@@ -1,43 +1,88 @@
 
-
- $(function() {
-    $('input[name="daterange"]').daterangepicker({
-      opens: 'left'
-    }, function(start, end, label) {
-      console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
-    });
-  });
+  
 
 
-  $(function()
-{
-$('.slider').on('input change', function(){
-          $(this).next($('.slider_label')).html(this.value);
+    $(function() {
+        $('input[name="daterange"]').daterangepicker({
+          opens: 'left'
+        }, function(start, end, label) {
+          console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
         });
-      $('.slider_label').each(function(){
-          var value = $(this).prev().attr('value');
-          $(this).html(value);
-        });  
-})
+      });
+    
+    
+      $(function()
+    {
+    $('.slider').on('input change', function(){
+              $(this).next($('.slider_label')).html(this.value);
+            });
+          $('.slider_label').each(function(){
+              var value = $(this).prev().attr('value');
+              $(this).html(value);
+            });  
+    })
+    
+    
+    $(function()
+    {
+        var options = [
+            "4’", "4’ 1&#8221;", "4’ 2&#8221;", "4’ 3&#8221;", "4’ 4&#8221;", "4’ 5&#8221;", "4’ 6&#8221;", "4’ 7&#8221;", "4’ 8&#8221;", "4’ 9&#8221;", "4’ 10&#8221;", "4’ 11&#8221;",
+            "5’", "5’ 1&#8221;", "5’ 2&#8221;", "5’ 3&#8221;", "5’ 4&#8221;", "5’ 5&#8221;", "5’ 6&#8221;", "5’ 7&#8221;", "5’ 8&#8221;", "5’ 9&#8221;", "5’ 10&#8221;", "5’ 11&#8221;",
+            "6’", "6’ 1&#8221;", "6’ 2&#8221;", "6’ 3&#8221;", "6’ 4&#8221;", "6’ 5&#8221;", "6’ 6&#8221;", "6’ 7&#8221;", "6’ 8&#8221;", "6’ 9&#8221;", "6’ 10&#8221;", "6’ 11&#8221;",
+            "7’", "7’ 1&#8221;", "7’ 2&#8221;", "7’ 3&#8221;", "7’ 4&#8221;", "7’ 5&#8221;", "7’ 6&#8221;", "7’ 7&#8221;", "7’ 8&#8221;", "7’ 9&#8221;", "7’ 10&#8221;", "7’ 11&#8221;",
+            "8’", "8’ 1&#8221;", "8’ 2&#8221;", "8’ 3&#8221;", "8’ 4&#8221;", "8’ 5&#8221;", "8’ 6&#8221;", "8’ 7&#8221;", "8’ 8&#8221;", "8’ 9&#8221;", "8’ 10&#8221;", "8’ 11&#8221;",
+            "9’", "9’ 1&#8221;", "9’ 2&#8221;", "9’ 3&#8221;", "9’ 4&#8221;", "9’ 5&#8221;", "9’ 6&#8221;", "9’ 7&#8221;", "9’ 8&#8221;", "9’ 9&#8221;", "9’ 10&#8221;", "9’ 11&#8221;",
+        ];
+        $('.slider2').on('input change', function(){
+            $('.slider_label2').html(options[this.value]);
+        });
+        $('.slider_label2').each(function(){
+            var value = $(this).prev().attr('value');
+            $(this).html(options[value]);
+        });
+    })
+
+//Clone campaign
 
 
-$(function()
-{
-    var options = [
-        "4’", "4’ 1&#8221;", "4’ 2&#8221;", "4’ 3&#8221;", "4’ 4&#8221;", "4’ 5&#8221;", "4’ 6&#8221;", "4’ 7&#8221;", "4’ 8&#8221;", "4’ 9&#8221;", "4’ 10&#8221;", "4’ 11&#8221;",
-        "5’", "5’ 1&#8221;", "5’ 2&#8221;", "5’ 3&#8221;", "5’ 4&#8221;", "5’ 5&#8221;", "5’ 6&#8221;", "5’ 7&#8221;", "5’ 8&#8221;", "5’ 9&#8221;", "5’ 10&#8221;", "5’ 11&#8221;",
-        "6’", "6’ 1&#8221;", "6’ 2&#8221;", "6’ 3&#8221;", "6’ 4&#8221;", "6’ 5&#8221;", "6’ 6&#8221;", "6’ 7&#8221;", "6’ 8&#8221;", "6’ 9&#8221;", "6’ 10&#8221;", "6’ 11&#8221;",
-        "7’", "7’ 1&#8221;", "7’ 2&#8221;", "7’ 3&#8221;", "7’ 4&#8221;", "7’ 5&#8221;", "7’ 6&#8221;", "7’ 7&#8221;", "7’ 8&#8221;", "7’ 9&#8221;", "7’ 10&#8221;", "7’ 11&#8221;",
-        "8’", "8’ 1&#8221;", "8’ 2&#8221;", "8’ 3&#8221;", "8’ 4&#8221;", "8’ 5&#8221;", "8’ 6&#8221;", "8’ 7&#8221;", "8’ 8&#8221;", "8’ 9&#8221;", "8’ 10&#8221;", "8’ 11&#8221;",
-        "9’", "9’ 1&#8221;", "9’ 2&#8221;", "9’ 3&#8221;", "9’ 4&#8221;", "9’ 5&#8221;", "9’ 6&#8221;", "9’ 7&#8221;", "9’ 8&#8221;", "9’ 9&#8221;", "9’ 10&#8221;", "9’ 11&#8221;",
-    ];
-    $('.slider2').on('input change', function(){
-        $('.slider_label2').html(options[this.value]);
-    });
-    $('.slider_label2').each(function(){
-        var value = $(this).prev().attr('value');
-        $(this).html(options[value]);
-    });
-})
+    $(document).ready(function(){
+        var field = $('.campaing-sec').clone().wrap('<div class="campaing-sec">').parent().html();
+        var numberOfFields = 1;
+        
+        $('#add').click(function(){
+            $('.campaing-div').append(field);
+            numberOfFields++
+        });
+        
+        $('.campaing-div').on('click', '.remove', function(e){
+          e.preventDefault();
+          if (numberOfFields > 1) {
+            $(this).parent('div').remove();
+            numberOfFields--
+          }
+         });
+        });
+
+
+
+//Clone sirecine
+
+
+  var sirecine = $('.sirecine-sec').clone().wrap('<div class="sirecine-sec">').parent().html();
+  var numberOfFields = 1;
+  
+  $('#addsire').click(function(){
+      $('.sirecine-cont').append(sirecine);
+      numberOfFields++
+  });
+  
+  $('.sirecine-cont').on('click', '.remove', function(e){
+    e.preventDefault();
+    if (numberOfFields > 1) {
+      $(this).parent('div').remove();
+      numberOfFields--
+    }
+   });
+
 
 
